fe07e33a5dfd319169ee92411c8b0ba5997fd0c6
Isabella Gonçalves Lopreti <lopreti@gmail.com>
2026-02-04 22:42:30 -0300

    aula02, tela de login Dark Mode

---
+++ aula-luiz/aula02/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <>
+      <main>
+        <form>
+          <h1>Bem Vindo(a)</h1>
+
+          <input type="text" placeholder="Digite o seu email" />
+          <input type="password" placeholder="Digite a sua senha" />
+
+          <button type="submit">Entrar</button>
+        </form>
+      </main>
+    </>
+  )
+}
+
+export default App
