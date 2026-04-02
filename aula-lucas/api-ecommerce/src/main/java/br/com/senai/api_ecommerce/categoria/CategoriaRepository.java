a389696302a2f1e956714635587555e8f4543728
Isabella Lopreti <lopretiisabella@gmail.com>
2026-04-02 10:57:49 -0300

    aula 02.04

---
+++ aula-lucas/api-ecommerce/src/main/java/br/com/senai/api_ecommerce/categoria/CategoriaRepository.java
@@ -0,0 +1,4 @@
+package br.com.senai.api_ecommerce.categoria;
+
+public interface CategoriaRepository {
+}
--- aula-lucas/api-ecommerce/src/main/java/br/com/senai/api_ecommerce/controller/CategoriaController.java
+++ aula-lucas/api-ecommerce/src/main/java/br/com/senai/api_ecommerce/controller/CategoriaController.java
@@ -1,4 +1,30 @@
-package br.com.senai.api_ecommerce;
+package br.com.senai.api_ecommerce.controller;
 
+import br.com.senai.api_ecommerce.categoria.Categoria;
+import br.com.senai.api_ecommerce.categoria.CategoriaRepository;
+import br.com.senai.api_ecommerce.categoria.DadosCadastroCategoria;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("categorias")
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrarCategoria(@RequestBody @Valid DadosCadastroCategoria dados) {
+      repository.save(new Categoria(dados));
+    }
+
+    @GetMapping
+    public List<Categoria> listarCategorias(){
+        return repository.findAll();
+    }
 }
